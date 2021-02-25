20841bd1080b56db813ca6bcb07be0d9fb20fb3c
thomasodev <thomas.matheus.queiroz@gmail.com>
2021-02-24 22:36:50 -0300

    Second day - NODE

--- Node/api/src/server.ts
+++ Node/api/src/server.ts
@@ -1,13 +1,11 @@
-import express from 'express';
+import "reflect-metadata";
+import express from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({message: "Hello World"})
-});
+app.use(express.json());
+app.use(router);
 
-app.post("/", (request, response) => {
-  return response.json({message: "os dados foram salvos com sucesso."})
-});
-
-app.listen(3333, ()=> console.log('Server is running'));
+app.listen(3333, () => console.log("Server is running"));
